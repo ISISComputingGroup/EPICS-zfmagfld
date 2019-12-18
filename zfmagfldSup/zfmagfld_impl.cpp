528ed641b6675c2acb5a5cf13fd8c073e8b3b2cb
Alistair McGann <alistair.mcgann@tessella.com>
2019-12-18 15:29:01 +0000

    Add error handling to c++

--- zfmagfldSup/zfmagfld_impl.cpp
+++ zfmagfldSup/zfmagfld_impl.cpp
@@ -18,108 +18,114 @@
 #include "zfmagfld.h"
 
 /**
- * Deallocates the arrays used in to do the matrix multiplication
+ * Multiplies the scaled, offset magnetometer data with the fixed sensor matrix.
+ * Inputs:
+ * (A, B, C) are the components of the measured, scaled field. Double precision
+ * D through L are the values of the 3x3 fixed sensor matrix, in row-major order. Double precision
+ * 
+ * Outputs:
+ * (VALA, VALB, VALC) are the components of the corrected field. Double precision
+ * VALD is the magnitute of the corrected field strength. Double precision
  */
-void free_array_data(gsl_vector* data_vector, gsl_matrix* sensor_matrix, gsl_vector* field_vector){
-    gsl_vector_free(data_vector);
-    gsl_matrix_free(sensor_matrix);
-    gsl_vector_free(field_vector);
-}
-
-
 long matrix_multiply_impl(aSubRecord *prec) 
 {
     /*
-     * Validate input data types
+     * Validate input and output data types
      */
 
     if (prec->fta != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type A", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input A", prec->name);
 		return 1;
 	}
 
     if (prec->ftb != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type B", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input B", prec->name);
 		return 1;
 	}
 
     if (prec->ftc != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type C", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input C", prec->name);
 		return 1;
 	}
 
     if (prec->ftd != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type D", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input D", prec->name);
 		return 1;
 	}
 
     if (prec->fte != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type E", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input E", prec->name);
 		return 1;
 	}
 
     if (prec->ftf != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type F", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input F", prec->name);
 		return 1;
 	}
 
     if (prec->ftg != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type G", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input G", prec->name);
 		return 1;
 	}
 
     if (prec->fth != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type H", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input H", prec->name);
 		return 1;
 	}
 
     if (prec->fti != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type I", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input I", prec->name);
 		return 1;
 	}
 
     if (prec->ftj != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type J", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input J", prec->name);
 		return 1;
 	}
 
     if (prec->ftk != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type K", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input K", prec->name);
 		return 1;
 	}
 
     if (prec->ftl != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect input argument type L", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type input L", prec->name);
 		return 1;
 	}
 
     if (prec->ftva != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect output argument type A", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type output VALA", prec->name);
 		return 1;
 	}
     
     if (prec->ftvb != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect output argument type B", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type output VALB", prec->name);
 		return 1;
 	}
 
     if (prec->ftvc != menuFtypeDOUBLE)
 	{
-        errlogSevPrintf(errlogMajor, "%s incorrect output argument type C", prec->name);
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type output VALC", prec->name);
+		return 1;
+	}
+
+    if (prec->ftvc != menuFtypeDOUBLE)
+	{
+        errlogSevPrintf(errlogMajor, "%s incorrect argument type output VALD", prec->name);
 		return 1;
 	}
 
@@ -143,7 +149,14 @@ long matrix_multiply_impl(aSubRecord *prec)
     gsl_matrix_set(sensor_matrix, 2, 1, *(epicsFloat64*)prec->k);
     gsl_matrix_set(sensor_matrix, 2, 2, *(epicsFloat64*)prec->l);
 
-    gsl_vector* field_vector = gsl_vector_calloc(3); 
+    // Allocate 3-element field vector, initialise to 0
+    gsl_vector* field_vector = gsl_vector_calloc(3);
+
+    // Allocate 1-element field strength variable, initialise to 0
+    double fieldStrength = 0.0;
+    double* field_strength = &fieldStrength;
+
+    int status = 0;
 
     /*
      * dgemv is a BLAS standard function to multiply a matrix and a vector, implemented in gsl.
@@ -155,25 +168,34 @@ long matrix_multiply_impl(aSubRecord *prec)
      * 1.0 and 0.0 are scaling factors, used here to keep or remove terms in the equation dgemv solves.
      * 
      */
-    gsl_blas_dgemv(CblasNoTrans, 1.0, sensor_matrix, data_vector, 0.0, field_vector);
-    
-    double fieldStrength = 0.0;
-    double* field_strength = &fieldStrength;
+    status = gsl_blas_dgemv(CblasNoTrans, 1.0, sensor_matrix, data_vector, 0.0, field_vector);
 
-    /* 
-     * Here we are calculating the magnitude of the field strength vector:
-     *    field strength^2 = field_x^2 + field_y^2 + field_z^2
-     * 
-     * The BLAS function ddot is used to calculate the dot product 
-     * of the field vector with itself.
-     */
-    gsl_blas_ddot(field_vector, field_vector, field_strength);
+    if (status) {
+        errlosSevPrintf(errlogMajor, "GSL matrix multiplication (DGEMV) error %d, %s", status, gsl_strerror(status));
+    } else {
+        /* 
+         * DGEMV worked, calculate the magnitude of the field strength vector:
+         *    field strength^2 = field_x^2 + field_y^2 + field_z^2
+         * 
+         * The BLAS function ddot is used to calculate the dot product 
+         * of the field vector with itself.
+         */
+        status |= gsl_blas_ddot(field_vector, field_vector, field_strength);
+    }
+
+    if (status){
+        errlosSevPrintf(errlogMajor, "GSL dot product (DDOT) error %d, %s", status, gsl_strerror(status));
+    }
 
     *(epicsFloat64*)prec->vala = gsl_vector_get(field_vector, 0); // X component
     *(epicsFloat64*)prec->valb = gsl_vector_get(field_vector, 1); // Y component
     *(epicsFloat64*)prec->valc = gsl_vector_get(field_vector, 2); // Z component
     *(epicsFloat64*)prec->vald = sqrt(*field_strength); // Magnitude of field vector
 
-    free_array_data(data_vector, sensor_matrix, field_vector);
-    return 0; /* process output links */
+    // Deallocate arrays
+    gsl_vector_free(data_vector);
+    gsl_matrix_free(sensor_matrix);
+    gsl_vector_free(field_vector);
+
+    return status; /* process output links or return error */
 }
